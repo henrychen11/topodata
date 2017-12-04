7f0be09b88f290376e863a1cc0c803b548c92de8
henrychen11 <henrychen1@gmail.com>
2017-12-03 19:42:32 -0800

    added TopoJSON to html

--- main/main.js
+++ main/main.js
@@ -1,148 +1,32 @@
 import * as d3 from 'd3';
+import * as topojson from 'topojson';
 
-//Test code
+var w = 1000;
+var h = 800;
+var margin = {	top: 20,	bottom: 20,	left: 20,	right: 20 };
 
-var w = 800;
-var h = 450;
-var margin = {
-	top: 60,
-	bottom: 80,
-	left: 80,
-	right: 80
-};
 var width = w - margin.left - margin.right;
 var height = h - margin.top - margin.bottom;
+
 var svg = d3.select("body").append("svg")
-			.attr("id", "chart")
-			.attr("width", w)
-			.attr("height", h);
-var chart = svg.append("g")
-			.classed("display", true)
-			.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-var colorScale = d3.scale.category10();
-var x = d3.scale.linear()
-		.domain(d3.extent(data, function(d){
-			return d.age;
-		}))
-		.range([0,width]);
-var y = d3.scale.linear()
-		.domain([1,5])
-		.range([height,0]);
-var tickValues = [18,25,32,39,46,53,60,67,74];
-var xAxis = d3.svg.axis()
-			.scale(x)
-			.tickValues(tickValues)
-			.orient("bottom");
-var xGridlines = d3.svg.axis()
-			.scale(x)
-			.tickValues(tickValues)
-			.tickSize(-height,-height)
-			.tickFormat("")
-			.orient("bottom");
-var yAxis = d3.svg.axis()
-			.scale(y)
-			.ticks(5)
-			.tickSize(20)
-			.tickFormat(function(d){
-				return d.toFixed(1);
-			})
-			.orient("left");
-var yGridlines = d3.svg.axis()
-				.scale(y)
-				.tickSize(-width,0,0)
-				.tickFormat("")
-				.orient("left");
-var responseScale = d3.scale.linear()
-					.domain(d3.extent(data, function(d){
-						return d.responses;
-					}))
-					.range([2,15]);
-function drawAxis(params){
-	if(params.initialize){
-		this.append("g")
-			.classed("gridline x", true)
-			.attr("transform", "translate(0," + height + ")")
-			.call(params.axis.gridlines.x);
-		this.append("g")
-			.classed("gridline y", true)
-			.attr("transform", "translate(0,0)")
-			.call(params.axis.gridlines.y);
-		this.append("g")
-			.classed("axis x", true)
-			.attr("transform", "translate(0," + height + ")")
-			.call(params.axis.x);
-		this.append("g")
-			.classed("axis y", true)
-			.attr("transform", "translate(0,0)")
-			.call(params.axis.y);
-	}
-}
-function plot(params){
-	drawAxis.call(this, params);
-	var self = this;
-	var donuts = d3.keys(params.data[0]).filter(function(d){
-		return d !== "age" && d !== "responses";
-	});
+						.attr("width", w)
+						.attr("height", h);
 
-	//enter() for <g>
-	this.selectAll(".donut")
-		.data(donuts)
-		.enter()
-			.append("g")
-			.attr("class", function(d){
-				return d;
-			})
-			.classed("donut", true);
+var path = d3.geoPath();
 
-	//update for <g>
-	this.selectAll(".donut")
-		.style("fill", function(d,i){
-			return colorScale(i);
-		});
 
-	donuts.forEach(function(donut){
-		var g = self.selectAll("g."+donut);
-		var arr = params.data.map(function(d){
-			return {
-				key: donut,
-				value: d[donut],
-				age: d.age,
-				responses: d.responses
-			};
-		});
-		//enter()
-		g.selectAll(".response")
-			.data(arr)
-			.enter()
-				.append("circle")
-				.classed("response", true);
-		//update
-		g.selectAll(".response")
-			.attr("r", function(d){
-				return responseScale(d.responses);
-			})
-			.attr("cx", function(d){
-				return x(d.age);
-			})
-			.attr("cy", function(d){
-				return y(d.value);
-			})
-		//exit()
-		g.selectAll(".response")
-			.data(arr)
-			.exit()
-			.remove();
-	});
-}
-plot.call(chart, {
-	data: data,
-	axis: {
-		x: xAxis,
-		y: yAxis,
-		gridlines:{
-			x: xGridlines,
-			y: yGridlines
-		}
-	},
-	initialize: true
-})
+// Template from https://bl.ocks.org/mbostock/4090848
+d3.json("https://d3js.org/us-10m.v1.json", function(error, us) {
+  if (error) throw error;
+
+  svg.append("g")
+      .attr("class", "states")
+    .selectAll("path")
+    .data(topojson.feature(us, us.objects.states).features)
+    .enter().append("path")
+      .attr("d", path);
+
+  svg.append("path")
+      .attr("class", "state-borders")
+      .attr("d", path(topojson.mesh(us, us.objects.states, function(a, b) { return a !== b; })));
+});
