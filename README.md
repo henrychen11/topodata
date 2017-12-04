e639014010b262d5473dfbaae879dcbd315fc838
henrychen11 <henrychen1@gmail.com>
2017-12-04 09:02:22 -0800

    added Readme for project proposal

--- README.md
+++ README.md
@@ -1,11 +1,19 @@
-## topodataValut
+## topodata
 
-#### [LIVE](sample)
+#### [LIVE](https://topodata.herokuapp.com/)
 
-### Background
-Text Here
+### Proposal
+This project is to show the average salary (wage) in the United States by state and by industry. I would like to add the ability to display historical data backing to 2011 if possible. Users will have the ability to filter by industry and also select the year. When they hover over each state, a tooltip will appear that shows the following items:
 
-### Technologies Used
+- State
+- Job Title
+- Max Salary
+- Median Salary
+- Min Salary
+
+![map](images/topodata_v1.png)
+
+### Technologies that will be used
 + D3.js
 + JavaScript
 
--- main/main.js
+++ main/main.js
@@ -8,28 +8,40 @@ const margin = {	top: 20,	bottom: 20,	left: 20,	right: 20 };
 const width = w - margin.left - margin.right;
 const height = h - margin.top - margin.bottom;
 
-const svg = d3.select("display").append("svg")
-						.attr("width", w)
-						.attr("height", h);
 
-const path = d3.geoPath();
 
-d3.queue()
-	.defer(d3.json, '../data/state.wage_data');
+const projection =  d3.geoAlbersUsa()
+										.translate([width/2, height/2])
+										.scale([500]);
+
+const path = d3.geoPath()
+							.projection(projection);
+
+//This is the main map element
+const svg = d3.select("map")
+	.append("svg")
+	.attr("width", w)
+	.attr("height", h);
+
+//This is the tooltip element
+
+const div = d3.select("map")
+								.append("div")
+								.classed("tooltip", true);
 
 
 // Template from https://bl.ocks.org/mbostock/4090848
-d3.json("https://d3js.org/us-10m.v1.json", function(error, us) {
-  if (error) throw error;
-
-  svg.append("g")
-      .attr("class", "states")
-    .selectAll("path")
-    .data(topojson.feature(us, us.objects.states).features)
-    .enter().append("path")
-      .attr("d", path);
-
-  svg.append("path")
-      .attr("class", "state-borders")
-      .attr("d", path(topojson.mesh(us, us.objects.states, function(a, b) { return a !== b; })));
-});
+// d3.json("https://d3js.org/us-10m.v1.json", function(error, us) {
+//   if (error) throw error;
+//
+//   svg.append("g")
+//       .attr("class", "states")
+//     .selectAll("path")
+//     .data(topojson.feature(us, us.objects.states).features)
+//     .enter().append("path")
+//       .attr("d", path);
+//
+//   svg.append("path")
+//       .attr("class", "state-borders")
+//       .attr("d", path(topojson.mesh(us, us.objects.states, function(a, b) { return a !== b; })));
+// });
