e5e958c9ab5baaf498818a9102eee4730024bfe9
henrychen11 <henrychen1@gmail.com>
2017-12-07 19:57:59 -0800

    fixed hover stats stats issue

--- README.md
+++ README.md
@@ -1,35 +1,36 @@
 ## topodata
 
-#### [LIVE](https://topodata.herokuapp.com/)
+#### [LIVE](https://topodata.henryhchen.com/)
 
 ### Proposal
-This project is to show the average salary (wage) in the United States by state and by industry. I would like to add the ability to display historical data backing to 2011 if possible. Users will have the ability to filter by industry and also select the year. When they hover over each state, a tooltip will appear that shows the following items:
+This project is to show the average salary (wage) in the United States by state. Users has the ability to display historical data from 2011. Users will have the ability to filter by industry and also select the year. When users hover over each state, a tooltip will appear that shows the following items:
 
 - State
-- Job Title
-- Max Salary
+- Average Salary
 - Median Salary
-- Min Salary
+- Total Employee
+- Average Salary (Hourly)
+- Median Salary (Hourly)
+- Relative Standard Error (%)
+
+### Technologies
+
++ D3.js
++ JavaScript
++ CSS / HTML
 
 ### Project Schedule
 
 Day 1: Research D3 Library and TopoJSON documentation. Get a US map rendered and display on SVG canvas. Learn how to read CSV data into hashes.
 
-Day 2: Finish Tooltip data dispay on hover and research on filtering data based on multiple criterias
+Day 2: Finish Tooltip data display on hover and research on filtering data based on multiple criteria
 
 Day 3: Adding multi-year data with a year slider that allows user to see the timeline trend
 
-Day 4: Adding CSS and Update Production README.
+Day 4: Adding CSS and a stats container that allows users to compare states.
 
-Day 5: Finish Prouction README and bugfixes.
+Day 5: Finish Production README and bug fixes.
+
+### Wireframe
 
 ![map](images/topodata_v1.png)
-
-### Technologies that will be used
-+ D3.js
-+ JavaScript
-
-### Features
-+ Feature
-+ Feature
-+ Feature
--- main/main.js
+++ main/main.js
@@ -49,10 +49,104 @@ d3.queue()
 	.defer(d3.csv, "data/data2016.csv")
 	.await(ready);
 
+	let states = [];
+	let hoveredState;
+	let averageSalarybyState = {};
+	let medianSalarybyState = {};
+	let totalEmployeebyState = {};
+	let hourMean = {};
+	let hourMedian = {};
+	let meanRSE = {};
+	let userSelect;
+
+	function ready(error, us, data) {
+	  if (error) throw error;
+			data.forEach( function(d) {
+				states.push(d.STATE);
+				averageSalarybyState[d.STATE] = Number(d.A_MEAN);
+				medianSalarybyState[d.STATE] = Number(d.A_MEDIAN);
+				totalEmployeebyState[d.STATE] = Number(d.TOT_EMP);
+				hourMean[d.STATE] = Number(d.H_MEAN);
+				hourMedian[d.STATE] = Number(d.H_MEDIAN);
+				meanRSE[d.STATE] = Number(d.MEAN_PRSE);
+			});
+
+		  svg.append("g")
+		      .attr("class", "states")
+				.selectAll("path")
+				.data(topojson.feature(us, us.objects.states).features)
+				.enter()
+					.append("path")
+					.attr("d", path)
+					.style("fill", function(d){
+						return colorScale(averageSalarybyState[d.properties.NAME]);
+					})
+					.on("mouseover", function(d, i){
+						d3.select(this).style("fill", "yellow").transition().duration(300).style("cursor", "pointer").style("display", "block");
+						//Updating Stats Box
+						hoveredState = d.properties.NAME;
+						updateStats(userSelect, hoveredState);
+						//Tooltip transitions
+						tooltip.transition().duration(350).style("opacity", 1);
+						tooltip.select(".state-abbr").html(d.properties.NAME);
+						tooltip.select(".average-salary").html("Avg. salary: $" + format(averageSalarybyState[d.properties.NAME]));
+						tooltip.select(".median-salary").html("Median Salary: $" + format(medianSalarybyState[d.properties.NAME]));
+						tooltip.select(".total-employee").html("Total Employee: " + format(totalEmployeebyState[d.properties.NAME]));
+						tooltip.select(".average-hour").html("Avg. salary (h): $" + format(hourMean[d.properties.NAME]));
+						tooltip.select(".median-hour").html("Median salary (h): $" + format(hourMedian[d.properties.NAME]));
+						tooltip.select(".error").html("% Relative Standard Error: " + format(meanRSE[d.properties.NAME]));
+					})
+					.on("mousemove", function(d, i){
+						return tooltip.style("top", (d3.event.pageY+20)+"px").style("left",(d3.event.pageX+15)+"px");
+					})
+					.on("mouseout", function(d, i){
+						d3.select(this)
+							.transition()
+							.duration(300)
+							.ease(d3.easeLinear)
+							.style("opacity", 1)
+							.style("fill", function(d){
+								return colorScale(averageSalarybyState[d.properties.NAME]);
+							});
+						tooltip.transition().duration(300).style("opacity", 0);
+						});
+
+		  svg.append("path")
+		      .attr("class", "state-borders")
+			  .attr("d", path(topojson.mesh(us, us.objects.states, function(a, b) { return a !== b; })));
+				const select = d3.select(".stats-container")
+													.append("select")
+													.attr("type", "select");
+
+				const options = select
+														.selectAll("option")
+														.data(states)
+														.enter()
+															.append("option")
+															.text(function(d) {return d;});
+
+						select.on("change", function() {
+							userSelect = d3.event.target.value;
+							updateStats(userSelect, hoveredState);
+						});
+				d3.select(".stats-container").append("div").attr("class", "state-label");
+				d3.select(".stats-container").append("div").attr("class", "custom-text").text("Hovered State")
+
+				d3.select(".stats-container").append("div").attr("class", "avg-salary");
+				d3.select(".stats-container").append("div").attr("class", "custom-text").text("Average Salary");
+
+				d3.select(".stats-container").append("div").attr("class", "med-salary").text("Median Salary");
+				d3.select(".stats-container").append("div").attr("class", "custom-text").text("Median Salary");
+
+				d3.select(".stats-container").append("div").attr("class", "tot-emp").text("Total Employees");
+				d3.select(".stats-container").append("div").attr("class", "custom-text").text("Total Employees");
+				updateStats(userSelect="Alabama", hoveredState="Alabama");
+	}
+
 // Template from https://bl.ocks.org/mbostock/4090848
 const colorScale = d3.scaleThreshold()
 .domain([30000, 33000, 36000, 39000, 42000, 45000, 48000, 51000, 54000, 57000, 60000])
-.range(['#90ee90','#81df80','#75ce73','#6cbf6b','#65ae67','#629c68','#618b6f','#62777c','#616194','#5745bb','#0000ff']);
+.range(['#f0e68c','#ddcf8b','#cab98a','#b7a388','#a48d86','#917883','#7d6380','#69507c','#533d78','#3b2a74','#191970']);
 
 
 const slider = d3.select(".slider")
@@ -71,7 +165,6 @@ function updateYear(year){
 function ready2(error, us, data) {
   if (error) throw error;
 	data.forEach( function(d) {
-		console.log("here");
 		states.push(d.STATE);
 		averageSalarybyState[d.STATE] = Number(d.A_MEAN);
 		medianSalarybyState[d.STATE] = Number(d.A_MEDIAN);
@@ -134,7 +227,7 @@ function ready2(error, us, data) {
 
 				select.on("change", function() {
 					userSelect = d3.event.target.value;
-					updateStats(d3.event.target.value, hoveredState);
+					updateStats(userSelect, hoveredState);
 				});
 }
 
@@ -142,117 +235,31 @@ const select = d3.select(".stats-container")
 									.append("select")
 									.attr("type", "select");
 
-let states = [];
-let hoveredState;
-let averageSalarybyState = {};
-let medianSalarybyState = {};
-let totalEmployeebyState = {};
-let hourMean = {};
-let hourMedian = {};
-let meanRSE = {};
-let userSelect;
-
-function ready(error, us, data) {
-  if (error) throw error;
-		data.forEach( function(d) {
-			states.push(d.STATE);
-			averageSalarybyState[d.STATE] = Number(d.A_MEAN);
-			medianSalarybyState[d.STATE] = Number(d.A_MEDIAN);
-			totalEmployeebyState[d.STATE] = Number(d.TOT_EMP);
-			hourMean[d.STATE] = Number(d.H_MEAN);
-			hourMedian[d.STATE] = Number(d.H_MEDIAN);
-			meanRSE[d.STATE] = Number(d.MEAN_PRSE);
-		});
-
-	  svg.append("g")
-	      .attr("class", "states")
-			.selectAll("path")
-			.data(topojson.feature(us, us.objects.states).features)
-			.enter()
-				.append("path")
-				.attr("d", path)
-				.style("fill", function(d){
-					return colorScale(averageSalarybyState[d.properties.NAME]);
-				})
-				.on("mouseover", function(d, i){
-					d3.select(this).style("fill", "yellow").transition().duration(300).style("cursor", "pointer").style("display", "block");
-					//Updating Stats Box
-					hoveredState = d.properties.NAME;
-					updateStats(userSelect, hoveredState);
-					//Tooltip transitions
-					tooltip.transition().duration(350).style("opacity", 1);
-					tooltip.select(".state-abbr").html(d.properties.NAME);
-					tooltip.select(".average-salary").html("Avg. salary: $" + format(averageSalarybyState[d.properties.NAME]));
-					tooltip.select(".median-salary").html("Median Salary: $" + format(medianSalarybyState[d.properties.NAME]));
-					tooltip.select(".total-employee").html("Total Employee: " + format(totalEmployeebyState[d.properties.NAME]));
-					tooltip.select(".average-hour").html("Avg. salary (h): $" + format(hourMean[d.properties.NAME]));
-					tooltip.select(".median-hour").html("Median salary (h): $" + format(hourMedian[d.properties.NAME]));
-					tooltip.select(".error").html("% Relative Standard Error: " + format(meanRSE[d.properties.NAME]));
-				})
-				.on("mousemove", function(d, i){
-					return tooltip.style("top", (d3.event.pageY+20)+"px").style("left",(d3.event.pageX+15)+"px");
-				})
-				.on("mouseout", function(d, i){
-					d3.select(this)
-						.transition()
-						.duration(300)
-						.ease(d3.easeLinear)
-						.style("opacity", 1)
-						.style("fill", function(d){
-							return colorScale(averageSalarybyState[d.properties.NAME]);
-						});
-					tooltip.transition().duration(300).style("opacity", 0);
-					});
-
-	  svg.append("path")
-	      .attr("class", "state-borders")
-		  .attr("d", path(topojson.mesh(us, us.objects.states, function(a, b) { return a !== b; })));
-			const select = d3.select(".stats-container")
-												.append("select")
-												.attr("type", "select");
-
-			const options = select
-													.selectAll("option")
-													.data(states)
-													.enter()
-														.append("option")
-														.text(function(d) {return d;});
-
-					select.on("change", function() {
-						updateStats(d3.event.target.value, hoveredState);
-					});
-			d3.select(".stats-container").append("div").attr("class", "state-label");
-			d3.select(".stats-container").append("div").attr("class", "custom-text").text("Hovered State")
-
-			d3.select(".stats-container").append("div").attr("class", "avg-salary");
-			d3.select(".stats-container").append("div").attr("class", "custom-text").text("Average Salary");
-
-			d3.select(".stats-container").append("div").attr("class", "med-salary").text("Median Salary");
-			d3.select(".stats-container").append("div").attr("class", "custom-text").text("Median Salary");
-
-			d3.select(".stats-container").append("div").attr("class", "tot-emp").text("Total Employees");
-			d3.select(".stats-container").append("div").attr("class", "custom-text").text("Total Employees");
-			updateStats(userSelect, hoveredState);
-}
-
-function updateStats(state="Alabama", state2="Alabama"){
-	// console.log('selected state ', state);
-	// console.log('hovered state ', state2);
-	console.log("here at update");
+function updateStats(selectedState, userHoveredState){
 	const st = d3.select(".state-label");
-	st.text(state);
+	st.text(userHoveredState);
 	st.exit().remove();
 
 	const avg_sal = d3.select(".avg-salary");
-	avg_sal.text("$" + format(averageSalarybyState[state2] - averageSalarybyState[state]));
+	avg_sal.text("$" + format(averageSalarybyState[selectedState] - averageSalarybyState[userHoveredState])).style("color", function(){
+		if ((medianSalarybyState[selectedState] - medianSalarybyState[userHoveredState]) > 0){
+			return "green";
+		} else if ((medianSalarybyState[selectedState] - medianSalarybyState[userHoveredState]) < 0) {
+			return "red";
+		}});
 	avg_sal.exit().remove();
 
 	const med_sal = d3.select(".med-salary");
-	med_sal.text("$" + format(medianSalarybyState[state2] - medianSalarybyState[state]));
+	med_sal.text("$" + format(medianSalarybyState[selectedState] - medianSalarybyState[userHoveredState])).style("color", function(){
+		if ((medianSalarybyState[selectedState] - medianSalarybyState[userHoveredState]) > 0){
+			return "green";
+		} else if ((medianSalarybyState[selectedState] - medianSalarybyState[userHoveredState]) < 0) {
+			return "red";
+		}});
 	med_sal.exit().remove();
 
 	const tot_emp = d3.select(".tot-emp");
-	tot_emp.text(format(totalEmployeebyState[state2] - totalEmployeebyState[state]));
+	tot_emp.text(format(totalEmployeebyState[selectedState] - totalEmployeebyState[userHoveredState]));
 	tot_emp.exit().remove();
 }
 
@@ -270,8 +277,8 @@ const legend = sideBar.append("g")
 legend.append("div")
 .attr("class", "legend-text")
 .text(function(d) {
-return "$" + d3.format(",")(d);
-});
+	return "$" + d3.format(",")(d);
+	});
 
 legend.append("div")
 .attr("class", "legend-color")
@@ -288,8 +295,6 @@ d3.select(".stats-container")
 	.append("div")
 	.attr("class", "stats-label")
 	.text("Variance From: ");
-
-
 
 //Footer
 d3.select(".navbar")
